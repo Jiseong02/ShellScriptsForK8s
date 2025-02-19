787a20b5030a54c09729ceec5125a23e54386f62
Jiseong <pupajusang01@gmail.com>
2025-02-19 23:00:38 +0900

    Update Base.sh

--- Base.sh
+++ Base.sh
@@ -59,6 +59,4 @@ sudo apt-mark hold kubelet kubeadm kubectl
 echo "Initiating kubeadm"
 sudo systemctl enable --now kubelet
 kubeadm init
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+export KUBECONFIG=/etc/kubernetes/admin.conf
