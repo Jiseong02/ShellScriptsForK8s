ceef742f6d7bfe4169611935067ddd8b56b91340
Jiseong <pupajusang01@gmail.com>
2025-02-21 13:04:55 +0900

    Update 0_set_enviroment.sh

--- Master/0_set_enviroment.sh
+++ Master/0_set_enviroment.sh
@@ -3,12 +3,19 @@ set -e
 
 echo "===0. Environment settings==="
 
+echo "Disabling Swap."
 cd /
 sudo apt-get update
 sudo swapoff -a
 sudo sed -i '/\sswap\s/s/^/#/' /etc/fstab
 
-echo "Migrating Systemd to Cgroup v2 from Cgroup v1"
+swap_units=$(systemctl --type=swap --no-legend --all | awk '{print $1}')
+for unit in $swap_units; do
+  echo "Masking swap unit: $unit"
+  sudo systemctl mask "$unit"
+done
+
+echo "Migrating Systemd to Cgroup v2 from Cgroup v1."
 sudo sed -i 's/GRUB_CMDLINE_LINUX="/GRUB_CMDLINE_LINUX="systemd.unified_cgroup_hierarchy=1 /' /etc/default/grub
 sudo update-grub
 
