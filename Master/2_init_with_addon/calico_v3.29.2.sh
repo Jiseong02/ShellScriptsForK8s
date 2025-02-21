99b1d4968646df5e9ca22f56973f166dcd1a83e6
Jiseong <pupajusang01@gmail.com>
2025-02-21 16:18:37 +0900

    Update calico_v3.29.2.sh

--- Master/2_init_with_addon/calico_v3.29.2.sh
+++ Master/2_init_with_addon/calico_v3.29.2.sh
@@ -4,6 +4,10 @@ set -e
 echo "===0.Calico Installation==="
 sudo kubeadm init --pod-network-cidr=192.168.0.0/16
 
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.2/manifests/tigera-operator.yaml
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.2/manifests/custom-resources.yaml
 
