a314220ec231898736798c2276e26ea8707ae150
Jiseong <pupajusang01@gmail.com>
2025-02-23 19:46:03 +0900

    Update calico_v3.29.2.sh

--- Master/calico_v3.29.2.sh
+++ Master/calico_v3.29.2.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "===0.Calico Installation==="
-sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+if [ ! -f /etc/kubernetes/admin.conf ]; then
+  echo "Initiate Kubeadm First!"
+  echo "Example: sudo kubeadm init --apiserver-advertise-address 'your-gateway-ip' --pod-network-cidr=192.168.0.0/16"
+  exit 1
 
+echo "===0.Calico Installation==="
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
@@ -11,7 +14,4 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.2/manifests/tigera-operator.yaml
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.2/manifests/custom-resources.yaml
 
-echo "Waiting for all pods in calico-system namespaces to be in 'Running' state."
-kubectl get pods -n calico-system
-kubectl taint nodes --all node-role.kubernetes.io/control-plane-
-kubectl get nodes -o wide
+echo "Check All Nodes are "Running" before next steps."
