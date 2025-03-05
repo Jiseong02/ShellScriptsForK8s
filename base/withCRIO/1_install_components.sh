68f6da08465cb41a3cce3c2f6ab3a7f6f8e93834
Jiseong <pupajusang01@gmail.com>
2025-03-05 19:22:16 +0900

    Update 1_install_components.sh

--- base/withCRIO/1_install_components.sh
+++ base/withCRIO/1_install_components.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "===1. Container runtime installation===" 
+echo "===Kubernetes Components installation===" 
 
 echo "Enabling IPv4 packet forwarding."
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
@@ -27,33 +27,12 @@ curl -fsSL https://download.opensuse.org/repositories/isv:/cri-o:/stable:/v1.32/
 echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://download.opensuse.org/repositories/isv:/cri-o:/stable:/v1.32/deb/ /" |
     tee /etc/apt/sources.list.d/cri-o.list
 
-echo "Installing CRI-O"
+echo "Installing CRI-O, Kubelet, Kubeadm, Kubectl"
 apt-get update
 apt-get install -y cri-o
 systemctl start crio.service
+apt-get install -y cri-o kubelet kubeadm kubectl
 
-echo "Installing runc."
-wget https://github.com/opencontainers/runc/releases/download/v1.2.5/runc.amd64
-install -m 755 runc.amd64 /usr/local/sbin/runc
-rm runc.amd64
-
-echo "Installing CNI plugins."
-wget https://github.com/containernetworking/plugins/releases/download/v1.6.2/cni-plugins-linux-amd64-v1.6.2.tgz
-mkdir -p /opt/cni/bin
-tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.6.2.tgz
-rm cni-plugins-linux-amd64-v1.6.2.tgz
-
-echo "===2. Kubernetes Components installation==="
-
-echo "Installing kubelet and kubeadm and kubectl."
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg
-sudo mkdir -p -m 755 /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
 sudo systemctl enable --now kubelet
 
 echo "Now you can make kubeadm init or join clusters. https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/"
