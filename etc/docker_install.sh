d384fb18c96befa854dd42baecedabe337c518a0
Jiseong02 <pupajusang01@gmail.com>
2025-03-05 18:19:52 +0900

    line break 방식 변경

--- etc/docker_install.sh
+++ etc/docker_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-
 echo "===Docker Installation==="
 echo "Removing Current Docker if it exists."
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
