f1b0dafe91d0866b4ee6dd27b60e549be17bb9ee
Jiseong <pupajusang01@gmail.com>
2025-03-06 12:53:57 +0900

    Update 1_install_components.sh

--- base/withCRIO/1_install_components.sh
+++ base/withCRIO/1_install_components.sh
@@ -20,14 +20,20 @@ echo "Adding the Kubernetes repository"
 curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key |
     gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-echo "Add the CRI-O repository"
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /" |
     tee /etc/apt/sources.list.d/kubernetes.list
 
-echo "Now you can make kubeadm init or join clusters. https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/"
+echo "Adding the CRI-O repository"
+curl -fsSL https://download.opensuse.org/repositories/isv:/cri-o:/stable:/$CRIO_VERSION/deb/Release.key |
+    gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://download.opensuse.org/repositories/isv:/cri-o:/stable:/$CRIO_VERSION/deb/ /" |
+    tee /etc/apt/sources.list.d/cri-o.list
 
 echo "Installing the packages"
 apt-get update
 apt-get install -y cri-o kubelet kubeadm kubectl
 
 systemctl start crio.service
+
+echo "Now you can make kubeadm init or join clusters. https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/"
